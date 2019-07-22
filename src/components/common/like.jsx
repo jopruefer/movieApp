b289f5ef9213b11e8e7df87b765592423c7b5f4a
Joshua Pruefer <jo.pruefer@icloud.com>
2019-07-22 03:44:56 +0200

    added

---
+++ src/components/common/like.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Like = (props) => {
+    return (
+        <i onClick={props.onClick} 
+            style={{cursor: "pointer"}} 
+            className={props.liked ? "fa fa-heart" : "fa fa-heart-o"} 
+            aria-hidden="true" />
+    );
+};
+
+export default Like;
